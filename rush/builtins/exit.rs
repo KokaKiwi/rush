d88ce52f9cbbbfe5cca56d4ca6df4d25beea7f2a
KokaKiwi <admin@kokabsolu.com>
2013-10-23 14:41:15 +0200

    Add an argument to exit.

--- rush/builtins/exit.rs
+++ rush/builtins/exit.rs
@@ -1,9 +1,22 @@
+use std::os;
 use rush::shared::*;
 
 pub static NAME: &'static str = "exit";
 
-pub fn builtin_exit(_args: &[~str]) -> Result<bool, CommandErr>
+pub fn builtin_exit(args: &[~str]) -> Result<bool, CommandErr>
 {
     println("Goodbye. :)");
+
+    let mut status = 0;
+    if args.len() == 1
+    {
+        status = match from_str(args[0]) {
+            Some(code) => code,
+            None => 1,
+        };
+    }
+
+    os::set_exit_status(status);
+
     Ok(true)
 }
